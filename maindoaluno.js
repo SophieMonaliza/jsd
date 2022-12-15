a81c00e64f327357c952bc957bb9b1c48214158b
SophieMonaliza <110426844+SophieMonaliza@users.noreply.github.com>
2022-12-15 11:03:23 -0300

    Add files via upload

--- maindoaluno.js
+++ maindoaluno.js
@@ -1,4 +1,4 @@
-var = SpeechRecognition = window.webkitSpeechRecognition;
+var  SpeechRecognition = window.webkitSpeechRecognition;
 
 
 var recognition = new SpeechRecognition();
@@ -15,7 +15,6 @@ function start()
 } 
 
 recognition.oneresult = function(event) {
-//Exiba esse evento no console
 console.log(event)
 
 var Content = event.results[0][0].transcript;
@@ -24,10 +23,58 @@ var Content = event.results[0][0].transcript;
     Textbox.innerHTML = Content;
     console.log(Content);
 
-     if(Content = "tire minha selfie"){
+     if(Content = "selfie"){
         console.log("tirando selfie ---");
 speak();
      }
 }
+function speak(){
+    var synth = window.speechSynthesis;
+    
+    speak_data = "Tirando sua selfie em 5 segundos";
 
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+ 
+    Webcam.attach(camera);
+
+    
+    
+    setTimeOut(function()
+    { 
+        take_selfie(); 
+        
+        save();
+        
+    }, 5000);
+}
+
+ 
+camera = document.getElementById("camera");
+Webcam.set({
+  
+    width:360,
+    height:250,
+    image_format : 'jpeg',
+    jpeg_quality:90
+});
+
+function take_selfie()
+{
+    Webcam.snap(function(data_uri) {
+
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+    });
+}
+
+
+function save()
+{
+  link = document.getElementById("link");
+  image = document.getElementById("selfie_image").src ;
+  link.href = image;
+  link.click();
+}
 
